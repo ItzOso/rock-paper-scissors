93ad33a3bf66fe26a9418911b8905094314af9a9
ItzOso <63934776+ItzOso@users.noreply.github.com>
2021-05-22 11:04:56 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,62 @@
+const winner = document.querySelector("#winner");
+const playerChose = document.querySelector("#player-chose");
+const aiChose = document.querySelector("#ai-chose");
+
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+
+rock.onclick = () => {
+  aiChooseAnswer("rock");
+};
+
+paper.onclick = () => {
+  aiChooseAnswer("paper");
+};
+
+scissors.onclick = () => {
+  aiChooseAnswer("scissors");
+};
+
+function chooseWinner(chosen, choice) {
+  playerChose.innerHTML = chosen;
+  aiChose.innerHTML = choice;
+  let combinations = {
+    c1: ["rock", "paper", false],
+    c2: ["paper", "rock", true],
+    c3: ["paper", "scissors", false],
+    c4: ["scissors", "paper", true],
+    c5: ["rock", "scissors", true],
+    c6: ["scissors", "rock", false],
+    c7: ["paper", "paper", null],
+    c8: ["rock", "rock", null],
+    c9: ["scissors", "scissors", null],
+  };
+  for (i = 1; i <= 9; i++) {
+    if (
+      combinations[`c${i}`][0] == chosen &&
+      combinations[`c${i}`][1] == choice
+    ) {
+      if (combinations[`c${i}`][2] == true) {
+        winner.innerHTML = "You";
+        break;
+      } else if (combinations[`c${i}`][2] == false) {
+        winner.innerHTML = "Computer";
+        break;
+      } else if (combinations[`c${i}`][2] == null) {
+        winner.innerHTML = "Draw";
+        break;
+      } else {
+        winner.innerHTML = "Error";
+        break;
+      }
+    }
+  }
+}
+
+function aiChooseAnswer(chosen) {
+  let randomNum = Math.floor(Math.random() * 2);
+  let options = ["rock", "paper", "scissors"];
+  let choice = options[randomNum];
+  chooseWinner(chosen, choice);
+}
